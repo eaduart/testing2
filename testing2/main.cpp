6a84ee133f59d502f681e0c53a30ea36c559b419
eaduart <eaduart@eaduarts-MacBook-Air.local>
2014-03-02 17:09:23 -0800

    Initial Commit

---
+++ testing2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  testing2
+//
+//  Created by eaduart on 3/2/14.
+//  Copyright (c) 2014 eaduart. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
